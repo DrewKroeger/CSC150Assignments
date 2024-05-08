0e15a70d1a4523e953159ed1d6a0546667d7a43a
Drew Kroeger <136010471+DrewKroeger@users.noreply.github.com>
2024-05-08 14:21:57 -0500

    Add files via upload

---
+++ AssignmentOne/coolStuff (1).java
@@ -0,0 +1,26 @@
+/**************************************
+ * CSC150
+ * Assignment 1
+ * Drew Kroeger
+ * Purpose is to write to CS in big letters made out of the letters they are making
+ **************************************/
+public class coolStuff 
+{
+	public static void main(String[]args)
+	{
+		System.out.println("**************************************************************************");
+		System.out.println("");
+		System.out.println("                     C C C                  S S S S            ! !");
+		System.out.println("                   C        C             S         S          ! !");
+		System.out.println("                 C                       S                     ! !");
+		System.out.println("                C                         S                    ! !");
+		System.out.println("                C                           S S S S            ! !");
+		System.out.println("                C                                   S          ! !");
+		System.out.println("                 C                                    S        ! !");
+		System.out.println("                   C        C               S        S            ");
+		System.out.println("                    C C C C                   S S S S          0 0");
+		System.out.println("");
+		System.out.println("**************************************************************************");
+		System.out.println("                   Computer Science is Cool Stuff!!!                      ");
+	}
+}//end of class coolStuff
